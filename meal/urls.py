8282ebc9a1cce27fd78e21e42116c0627541434a
BLADE <abdirahman.ahmed@student.moringaschool.com>
2021-10-12 09:14:37 +0300

    added requirements.txt file

--- meal/urls.py
+++ meal/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(('booking.urls', 'booking'), namespace='booking')),
-    path('accounts/', include('django.contrib.auth.urls')),
+   
     
 ]
